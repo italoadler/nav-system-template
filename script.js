8e13252e2bb088ea3a946428f0d160e155d695ac
Italo Adler <italoadler@users.noreply.github.com>
2023-12-06 10:57:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// Função para carregar o conteúdo das páginas de forma dinâmica
+function carregarPagina(pagina) {
+  fetch(`${pagina}.html`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro ao carregar a página.');
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('conteudo').innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+}
